d4dad5cf949da84fdfd296a8085647e7e58e26b8
Fiorella Castillo Larriviere <fcastillolarriviere@gmail.com>
2022-12-22 19:53:24 -0300

    CRUD 1

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,12 +8,20 @@ class MoviesController < ApplicationController
   end
 
   def show
+    result = RestClient.get("http://localhost:3001/api/v1/movies/#{params[:id]}")
+    @movies = JSON.parse(result)
   end
 
   def new
   end
 
   def create
+    result = RestClient.post("http://localhost:3001/api/v1/movies", { 'name' => params["name"], 'director' => params["director"] }.to_json, :content_type => :json, accept: => :json)
+
+    if result.code == 201
+      redirect_to "/movies/new", status: :unprocessable_entity
+    end
+
   end
 
   def edit
